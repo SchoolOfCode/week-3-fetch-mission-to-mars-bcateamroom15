efa3cbee16ea554f53c58b598b10fef29f398d06
DamonSeahash5 <oliverhopwood5@gmail.com>
2024-12-11 12:33:32 +0000

    accessed messages

--- launch-sequence.js
+++ launch-sequence.js
@@ -17,7 +17,8 @@ async function submitCode() {
   try {
     // Your investigation code should go here
     // Leave your lines of code in when you find something out, so that you can always come back to it and see how you got there
-    const response = await fetch("/api/logs");
+    const response = await fetch("/api/messages?to=8");
+    console.log(response);
     const data = await response.json();
     console.log(data);
   } catch (error) {
